5d6e81fdb1abe6060c4abb1434dc7ac03593b891
Andrea Natale <andreanatale85@proton.me>
2024-10-15 16:21:39 +0200

    Updates

--- app/components/CookieBanner.jsx
+++ app/components/CookieBanner.jsx
@@ -1,9 +1,11 @@
 "use client"
 
+import Script from "next/script"
+
 const CookieBanner = () => {
     return (
-        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a1037e13-2a10-473a-805b-cd81a2a4b371" type="text/javascript"></script>
-    )
+        <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a1037e13-2a10-473a-805b-cd81a2a4b371" data-blockingmode="auto" type="text/javascript"></Script>
+        )
 }
 
 export default CookieBanner
