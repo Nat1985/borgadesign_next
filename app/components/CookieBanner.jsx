49506aad6dd09bdf06991a9d37e5c8cb2cee7373
Andrea Natale <andreanatale85@proton.me>
2024-10-15 16:04:10 +0200

    Updates

--- app/components/CookieBanner.jsx
+++ app/components/CookieBanner.jsx
@@ -2,7 +2,7 @@
 
 const CookieBanner = () => {
     return (
-        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a1037e13-2a10-473a-805b-cd81a2a4b371" data-blockingmode="auto" type="text/javascript" strategy="afterInteractive"></script>
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a1037e13-2a10-473a-805b-cd81a2a4b371" type="text/javascript"></script>
     )
 }
 
