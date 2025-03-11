8fe4dea03cc6ef19114965147f4be84ea009a629
Andrea <andreanatale85@proton.me>
2025-03-11 10:20:57 +0100

    Gtm fixed

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -31,17 +31,31 @@ export default function RootLayout({ children }) {
       <head>
         <CookieBanner />
         {/* Google Tag Manager */}
-        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KB6JSM6P');</script>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KB6JSM6P');
+              `
+          }}
+        />
         {/* End Google Tag Manager */}
       </head>
       <body className={montserrat.className}>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB6JSM6P"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB6JSM6P"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `
+          }}
+        />
         {/* End Google Tag Manager (noscript) */}
         <Navbar />
         {children}
